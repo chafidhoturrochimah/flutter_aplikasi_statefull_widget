35de661989728f5a28d617cc028e732cc3c82cd9
chafidhoturrochimah <chafidhotur96@gmail.com>
2021-02-22 20:34:11 +0700

    konversi desain ke widget

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-//import 'package:flutter/services.dart';
+import 'package:flutter/services.dart';
 
 void main() {
   runApp(MyApp());
@@ -10,29 +10,32 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
-        visualDensity: VisualDensity.adaptivePlatformDensity,
-      ),
-      home: Scaffold(
-        appBar: AppBar(
-          title: Text("Konverter Suhu"),
+        title: 'Flutter Demo',
+        theme: ThemeData(
+          primarySwatch: Colors.blue,
+          visualDensity: VisualDensity.adaptivePlatformDensity,
         ),
-        body: Container(
-          margin: EdgeInsets.all(8),
-          child: TextFormField(
-            decoration: InputDecoration(
-              enabledBorder: OutlineInputBorder(
-                borderSide: BorderSide(color: Colors.grey),
-              ),
-              focusedBorder: OutlineInputBorder(
-                borderSide: BorderSide(color: Colors.blue),
-              ),
+        home: Scaffold(
+          appBar: AppBar(
+            title: Text("Konverter Suhu"),
           ),
-        ),
-      ),
-    )
-    );
+          body: Container(
+            margin: EdgeInsets.all(8),
+            child: Column(
+              children: [
+                TextFormField(
+                  keyboardType: TextInputType.number,
+                  inputFormatters: <TextInputFormatter>[
+                    // ignore: deprecated_member_use
+                    WhitelistingTextInputFormatter.digitsOnly
+                  ],//Only numbers can be entered
+                  decoration: InputDecoration(
+                    hintText: 'Masukkan Suhu Dalam Celcius',
+                  ),
+                ),
+              ],
+            ),
+          ),
+        ));
   }
 }
