1a123e7f256b5d2f64993e1c1068f5c6e92cbeb6
chafidhoturrochimah <chafidhotur96@gmail.com>
2021-02-25 11:40:11 +0700

    Konversi desain widget

--- lib/main.dart
+++ lib/main.dart
@@ -33,9 +33,52 @@ class MyApp extends StatelessWidget {
                     hintText: 'Masukkan Suhu Dalam Celcius',
                   ),
                 ),
-              ],
-            ),
+
+                Expanded(
+                  child: Row (
+                    mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                    children: <Widget> [
+                      Column(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: <Widget> [
+                          Text('Suhu dalam Kelvin',
+                          style: TextStyle(height: 1.5)
+                          ),
+                          //Text('$_kelvin', style: TextStyle(fontSize: 20)),
+                        ],
+                      ),
+
+                      Column(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: <Widget> [
+                          Text('Suhu dalam Reamur',
+                          style: TextStyle(height: 1.5)
+                          ),
+                          //Text('$_reamur', style: TextStyle(fontSize: 20)),
+                        ],
+                      ),
+                    ],
+                  ),
+                ),
+
+                SizedBox(
+                  width: 420,
+                  height: 50,
+                  child: (
+                    // ignore: deprecated_member_use
+                    RaisedButton(
+                      onPressed: (){},
+                      color: Colors.blue,
+                      hoverColor: Colors.blueGrey,
+                      textColor: Colors.white,
+                      child: const Text('Konversi Suhu'),
+                    )
+                  ),
+                )  
+            ],
           ),
-        ));
+        ),
+      )
+    );
   }
 }
